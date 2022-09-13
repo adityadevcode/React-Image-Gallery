229812818cda4bec2607dbc691ea2b5e1ab88c04
addycoderepo <adityagitcode@gmail.com>
2022-09-13 16:26:26 -0400

    Api is not valid

---
+++ imagegalleryreact/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Gallery from './Components/Gallery';
+import Data from './Components/Data';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Gallery />
+      <Data />
+    </React.Fragment>
+  );
+}
+
+export default App;
